8a82e07b3db3e9ffb9b00f22b55e568e1483dac4
yutolx12 <yunanta86@gmail.com>
2022-12-05 15:39:29 +0700

    add mqtt connection

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:mop_green/pages/pages.dart';
+import 'package:mop_green/shared/shared.dart';
+import 'package:http/http.dart' as http;
+import 'package:mqtt_client/mqtt_client.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          primarySwatch: Colorpalette.purpleColor,
+          primaryColor: primaryColor,
+          canvasColor: Colors.transparent),
+      home: const selamatdatang(),
+    );
+  }
+}
